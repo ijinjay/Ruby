edd678037a95254a075950075c1ec3a8cd594479
Jin Jay <jinjaysnow@gmail.com>
2014-05-07 22:38:04 +0800

    md5 & base64 加密

--- md5.rb
+++ md5.rb
@@ -1,3 +1,9 @@
 require 'digest/md5'
 puts Digest::MD5.hexdigest('jinjaysnow')
 
+# base64
+require 'base64'
+code = Base64.encode64('jinjaysnow')
+source = Base64.decode64(code)
+puts code
+puts source
